8919a1cf5ab3fb211a1c48ff0b0e3eab22c0f11c
2019001967 <102298990+kkm333@users.noreply.github.com>
2024-09-04 14:51:04 +0900

    feat: history

--- prix/homepage/src/main/java/com/prix/homepage/backend/history/controller/HistoryController.java
+++ prix/homepage/src/main/java/com/prix/homepage/backend/history/controller/HistoryController.java
@@ -1,34 +1,48 @@
 package com.prix.homepage.backend.history.controller;
 
-import com.prix.homepage.backend.history.service.HistoryService;
+import com.prix.homepage.backend.admin.entity.SearchLog;
+import com.prix.homepage.backend.history.mapper.HistoryMapper;
 import com.prix.homepage.frontend.controller.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class HistoryController extends BaseController {
 
+
+    private final HistoryMapper historyMapper;
+
     @Autowired
-    private HistoryService historyService;
+    public HistoryController(HistoryMapper historyMapper) {
+        this.historyMapper = historyMapper;
+    }
 
     /**
      * 특정 사용자의 히스토리 페이지를 반환합니다.
      *
-     * @param session 현재 세션 객체.
      * @param model 뷰에 데이터를 전달하기 위한 모델 객체.
-     * @return 히스토리 페이지 뷰 이름.
+     * @return 히스토리 페이지
      */
-    @GetMapping("/history")
-    public String showHistory(HttpSession session, Model model) {
 
-//        List<SearchLog> searchLog = historyService.getHistoryByUserId(userId);
-//
-//        model.addAttribute("historyList", searchLog);
+    @GetMapping("/searchlog")
+    public String getHistory(Model model) {
 
-        return "history.html";
+        final int pageSize = 50;
+
+//        int id = (int)model.getAttribute(SESSION_KEY_ID);
+        int id = 778;
+
+        List<SearchLog> searchLogs = historyMapper.findSearchLogs(id);
+
+        model.addAttribute("searchLogs", searchLogs);
+        model.addAttribute("pageSize", pageSize);
+
+        return "livesearch/history";
     }
 }
--- prix/homepage/src/main/java/com/prix/homepage/backend/history/mapper/HistoryMapper.java
+++ prix/homepage/src/main/java/com/prix/homepage/backend/history/mapper/HistoryMapper.java
@@ -1,8 +1,9 @@
 package com.prix.homepage.backend.history.mapper;
 
 
-import com.prix.homepage.backend.history.domain.SearchLog;
+import com.prix.homepage.backend.admin.entity.SearchLog;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -13,9 +14,17 @@ public interface HistoryMapper {
     /**
      * 특정 사용자의 히스토리 레코드를 조회합니다.
      *
-     * @param userId 히스토리 레코드를 조회할 사용자의 ID.
+     * @param id 히스토리 레코드를 조회할 사용자의 ID.
      * @return History 객체 리스트.
      */
-    @Select("SELECT title, date, msfile, db, result, engine FROM px_search_log WHERE user_id = #{userId} ORDER BY date DESC, id DESC LIMIT 50")
-    List<SearchLog> findSearchLogsByUserId(int userId);
+    @Select("SELECT l.title, l.date, " +
+            "COALESCE(msData.name, '') as msFile, " +
+            "COALESCE(dbData.name, '') as dbFile, " +
+            "l.result, l.engine, " +
+            "l.id " +
+            "FROM px_search_log l " +
+            "LEFT JOIN px_data msData ON l.msfile = msData.id " +
+            "LEFT JOIN px_data dbData ON l.db = dbData.id " +
+            "WHERE user_id = #{id} ORDER BY date DESC, id DESC LIMIT 50")
+    List<SearchLog> findSearchLogs(@Param("id") int id);
 }
