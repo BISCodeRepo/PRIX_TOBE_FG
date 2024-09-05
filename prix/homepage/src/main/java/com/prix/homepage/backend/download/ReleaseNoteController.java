f8061034ecbeeb19ec4f018becbe5f4a74b7328f
2019001967 <102298990+kkm333@users.noreply.github.com>
2024-09-05 10:50:49 +0900

    feat: download releasenote

---
+++ prix/homepage/src/main/java/com/prix/homepage/backend/download/ReleaseNoteController.java
@@ -0,0 +1,53 @@
+package com.prix.homepage.backend.download;
+
+import com.prix.homepage.frontend.controller.BaseController;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+@Controller
+@RequestMapping("/download/releasenote")
+public class ReleaseNoteController extends BaseController {
+
+    @GetMapping()
+    public ResponseEntity<Resource> getReleaseNote(
+            @RequestParam(name = "fileName", required = false) String fileName,
+            @RequestHeader(value = "Referer", required = false) String referer
+            ) throws IOException {
+
+        // 파일 이름이 없을 경우 기본 파일 설정
+        if (fileName == null || fileName.isEmpty()) {
+            fileName = "MutCombinator";  // 기본 파일 이름
+        }
+
+        // 파일 경로 설정 (동적으로 전달받은 파일 이름 사용)
+        String filePath = "static/releasenote/" + fileName + "_ReleaseNote.txt";
+        Resource resource = new ClassPathResource(filePath);
+
+        // 파일이 존재하지 않으면 예외 처리
+        if (!resource.exists()) {
+            if (referer != null && !referer.isEmpty()) {
+                return ResponseEntity.status(302).header("Location", referer).build();
+            }
+            return ResponseEntity.status(302).header("Location", "/").build();
+        }
+
+        // 파일의 MIME 타입 설정
+        String contentType = Files.probeContentType(resource.getFile().toPath());
+
+        // 파일을 HTTP 응답으로 반환
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, contentType != null ? contentType : MediaType.TEXT_PLAIN_VALUE)
+                .body(resource);
+    }
+
+
+
+}
