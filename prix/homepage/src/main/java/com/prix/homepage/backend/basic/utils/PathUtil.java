c6c703208a5cf67f624d402648805c10f8d51dcb
2019001967 <102298990+kkm333@users.noreply.github.com>
2024-09-14 12:01:22 +0900

    feat: path util base path 추가

--- prix/homepage/src/main/java/com/prix/homepage/backend/basic/utils/PathUtil.java
+++ prix/homepage/src/main/java/com/prix/homepage/backend/basic/utils/PathUtil.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Component;
 public class PathUtil {
 
     private static String libraryPath;
-    public final static String PATH_CONFIG =
-            "/usr/local/server/apache-tomcat-8.0.14/webapps/ROOT/config/";
-    public final static String PATH_SW =
-            "/usr/local/server/apache-tomcat-8.0.14/webapps/ROOT/download/software_archive/";
-//            "D:\\code\\GitHub\\PRIX_TOBE_FG2\\prix\\homepage\\src\\main\\resources\\static\\software_archive";
-//    "D:\\";
 
+//    실제 경로에 맞게 수정해야합니다.
+    public final static String BASE_PATH =
+            "/usr/local/server/apache-tomcat-8.0.14/webapps/ROOT";
+
+    public final static String PATH_CONFIG =
+            BASE_PATH + "/config/";
+    public final static String PATH_SW =
+            BASE_PATH + "/software_archive";
     public final static String PATH_SW_RELEASE =
-//            "D:\\release\\";
-            "/usr/local/server/apache-tomcat-8.0.14/webapps/ROOT/download/software_archive/release/";
+            PATH_SW + "/release";
 
 
 
