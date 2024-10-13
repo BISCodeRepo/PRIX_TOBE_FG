5f73287a2d66185e8bcca898806503917fdd9cfa
2019001967 <102298990+kkm333@users.noreply.github.com>
2024-10-13 23:16:46 +0900

    feat: 운영서버용 수정

--- prix/homepage/src/main/java/com/prix/homepage/backend/basic/authorization/SessionLevelInterceptor.java
+++ prix/homepage/src/main/java/com/prix/homepage/backend/basic/authorization/SessionLevelInterceptor.java
@@ -12,17 +12,17 @@ public class SessionLevelInterceptor implements HandlerInterceptor {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        HttpSession session = request.getSession(false);
-//        if (session == null) {
-//            response.sendRedirect(request.getContextPath() + "/");
-//            return false;
-//        }
-//        Integer userLevel = (Integer) session.getAttribute("level");
-//
-//        if (userLevel == null || userLevel != 2) {
-//            response.sendRedirect(request.getContextPath() + "/");
-//            return false;
-//        }
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.sendRedirect(request.getContextPath() + "/");
+            return false;
+        }
+        Integer userLevel = (Integer) session.getAttribute("level");
+
+        if (userLevel == null || userLevel != 2) {
+            response.sendRedirect(request.getContextPath() + "/");
+            return false;
+        }
 
         return true;
     }
